4d1220974d362e8fd32e7d22a79e47b07ac61a99
David-Huynh <huynh.david.work@gmail.com>
2024-05-17 22:35:02 -0400

    updated import

--- app/routes/_auth+/onboarding_.$provider.server.ts
+++ app/routes/_auth+/onboarding_.$provider.server.ts
@@ -1,7 +1,7 @@
 import { invariant } from '@epic-web/invariant'
 import { redirect } from '@remix-run/node'
 import { verifySessionStorage } from '#app/utils/verification.server.ts'
-import { onboardingEmailSessionKey } from './onboarding.tsx'
+import { onboardingEmailSessionKey } from './onboarding_.$provider.tsx'
 import { type VerifyFunctionArgs } from './verify.server.ts'
 
 export async function handleVerification({ submission }: VerifyFunctionArgs) {
